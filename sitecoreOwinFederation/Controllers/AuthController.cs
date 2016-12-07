44091e549b1b8ce7729f6626a22d06e80c4a732e
Diego Saavedra San Juan <diego.saavedra.san.juan@liu.se>
2016-12-07 15:55:47 +0100

    Implemented new features for redirection to originating page and to edit in EE for selected roles. Removed unused references. Fixed logout when user is Authenticated in Sitecore (needed for real users).

--- sitecoreOwinFederation/Controllers/AuthController.cs
+++ sitecoreOwinFederation/Controllers/AuthController.cs
@@ -1,12 +1,17 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Owin.Security;
+using Sitecore;
 using Sitecore.Analytics;
+using Sitecore.Configuration;
 using Sitecore.Diagnostics;
 using Sitecore.Security.Authentication;
+using Sitecore.StringExtensions;
+using Sitecore.Web;
+using Sitecore.Web.Authentication;
 using SitecoreOwinFederator.Authenticator;
-using SitecoreOwinFederator.Models;
 using SitecoreOwinFederator.Pipelines.HttpRequest;
 
 namespace SitecoreOwinFederator.Controllers
@@ -28,19 +33,43 @@ namespace SitecoreOwinFederator.Controllers
       var principal = IdentityHelper.GetCurrentClaimsPrincipal();
 
       System.Web.HttpContext.Current.GetOwinContext().Authentication.Challenge();
+      Log.Debug("ADFSAuth Owin user name: " + System.Web.HttpContext.Current.GetOwinContext().Authentication.User.Identity.Name);
 
       var ctx = Tracker.Current.Session;
       // Login the sitecore user with the claims identity that was provided by identity ticket
       LoginHelper loginHelper = new LoginHelper();
       loginHelper.Login(principal);
 
+      Log.Debug("ADFSAuth: After log in in AuthController, user is " + Context.User.GetLocalName());
+      Log.Debug("ADFSAuth: After log in Owin user name: " + System.Web.HttpContext.Current.GetOwinContext().Authentication.User.Identity.Name);
+
+      System.Web.HttpContext.Current.User = Sitecore.Context.User;
       ctx = Tracker.Current.Session;
 
       // temporary code to show user claims, while there is a sitecore user object as
       //UserClaimsModel ucm = new UserClaimsModel();
       //ucm.Claims = ((ClaimsPrincipal)principal).Claims;
       //return View(ucm);
-      return Redirect("/");
+      string redirect = "/";
+      if (Settings.GetBoolSetting(Constants.RedirectToCurrentLocationSettingName,false)
+        && !WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName).IsNullOrEmpty() &&
+          !WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName).Contains("logout"))
+      {
+        Log.Debug("ADFSAuth: In AuthController login, found adfsSavePath cookie with value: " + WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName));
+        redirect = WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName);
+      }
+
+      string rolesToRedirectToEdit = Settings.GetSetting(Constants.RolesToRedirectToEditSettingName, "");
+      Log.Debug("ADFSAuth: In AuthController login, rolesToRedirectToEdit is :" + rolesToRedirectToEdit);
+      if (!rolesToRedirectToEdit.IsNullOrEmpty() &&
+          rolesToRedirectToEdit.Split('|').Any(role => Context.User.IsInRole(role)))
+      {
+        Log.Debug("ADFSAuth: In AuthController login, user matched roles to redirect to edit");
+        redirect += Constants.SitecoreStartEditingParameter;
+      }        
+
+      Log.Debug("ADFSAuth: In AuthController login, redirecting to " + redirect);
+      return Redirect(redirect);
     }
 
     /// <summary>
@@ -51,12 +80,35 @@ namespace SitecoreOwinFederator.Controllers
     {
       Log.Debug("ADFSAuth AuthController Logout");
 
-      AuthenticationProperties properties = new AuthenticationProperties();
-      properties.RedirectUri = "/login";
-      properties.AllowRefresh = false;
-      AuthenticationManager.Logout();
-      Request.GetOwinContext().Authentication.SignOut(properties);
-      //return View();
+      if (Request.IsAuthenticated)
+      {
+        string redirect = "/";
+        if (!WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName).IsNullOrEmpty() && Settings.GetBoolSetting(Constants.RedirectToCurrentLocationSettingName, false))
+          redirect = WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName);
+
+        AuthenticationProperties properties = new AuthenticationProperties();
+        
+        properties.RedirectUri = "https://" + Context.Site.TargetHostName + redirect;        
+        properties.AllowRefresh = false;
+        AuthenticationManager.Logout();
+        foreach (AuthenticationProvider authenticationProvider in AuthenticationManager.Providers)
+        {
+          Log.Debug("ADFSAuth: Logging out user " + Context.User.Name + " in provider: " + authenticationProvider.Name);
+          authenticationProvider.Logout();
+        }
+
+        // Log out from Sitecore for real as we are using real users
+        Session.Abandon();
+        if (!TicketManager.GetCurrentTicketId().IsNullOrEmpty())
+          TicketManager.RemoveTicket(TicketManager.GetCurrentTicketId());
+        WebUtil.SetCookieValue(Constants.SitecoreUserTicketCookieName, "",DateTime.Now.AddDays(-1));
+
+        Request.GetOwinContext().Authentication.SignOut(properties);
+        
+        Log.Debug("ADFSAuth: In AuthController logout, redirecting to " + redirect);
+        return Redirect(redirect);
+
+      }
       return Redirect("/");
     }
   }
