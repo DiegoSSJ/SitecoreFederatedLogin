db8911b9d73058bbf129b2514ae8642f67964d41
Diego Saavedra San Juan <diego.saavedra.san.juan@liu.se>
2016-12-22 15:02:40 +0100

    Fixed exception on garbage collector, we log that now. It can happen if the connection string is not present. New package version.

--- sitecoreOwinFederation/Authenticator/EFAuthStore.cs
+++ sitecoreOwinFederation/Authenticator/EFAuthStore.cs
@@ -5,9 +5,11 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Sitecore.Diagnostics;
 
 /// <summary>
 /// This implementation is a literal copy paste from Vittorio Bertocci
@@ -15,123 +17,131 @@ using System.Threading.Tasks;
 /// </summary>
 namespace SitecoreOwinFederator.Authenticator
 {
-    /// <summary>
-    /// Model for use with the session authentication store. data will be stored into database
-    /// </summary>
-    public class AuthSessionEntry
+  /// <summary>
+  /// Model for use with the session authentication store. data will be stored into database
+  /// </summary>
+  public class AuthSessionEntry
+  {
+    [Key]
+    public string Key { get; set; }
+    public DateTimeOffset? ValidUntil { get; set; }
+    public string TicketString { get; set; }
+  }
+
+  public class SQLAuthSessionStoreContext : DbContext
+  {
+    public SQLAuthSessionStoreContext(string init = "AuthSessionStoreContext")
+        : base(init)
+    { }
+    public DbSet<AuthSessionEntry> Entries { get; set; }
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
     {
-        [Key]
-        public string Key { get; set; }
-        public DateTimeOffset? ValidUntil { get; set; }
-        public string TicketString { get; set; }
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
+  }
+
+  public class SqlAuthSessionStoreInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SQLAuthSessionStoreContext>
+  {
+  }
+
+  /// <summary>
+  /// Session store to be used with the OWIN cookie authentication middleware
+  /// </summary>
+  public class SqlAuthSessionStore : IAuthenticationSessionStore
+  {
+    private string _connectionString;
+    private TicketDataFormat _formatter;
+    private Timer _gcTimer;
+    public SqlAuthSessionStore(TicketDataFormat tdf, string cns = "AuthSessionStoreContext")
+    {
+      _connectionString = cns;
+      _formatter = tdf;
+      _gcTimer = new Timer(new TimerCallback(GarbageCollect), null, TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(15));
     }
 
-    public class SQLAuthSessionStoreContext : DbContext
+    private void GarbageCollect(object state)
     {
-        public SQLAuthSessionStoreContext(string init = "AuthSessionStoreContext")
-            : base(init)
-        { }
-        public DbSet<AuthSessionEntry> Entries { get; set; }
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+      DateTimeOffset now = DateTimeOffset.Now.ToUniversalTime();
+      try
+      {
+        using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
         {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+          foreach (var entry in _store.Entries)
+          {
+            var expiresAt = _formatter.Unprotect(entry.TicketString).Properties.ExpiresUtc;
+            if (expiresAt < now)
+            {
+              _store.Entries.Remove(entry);
+            }
+          }
+          _store.SaveChanges();
         }
+      }
+      // Handle wrongly configured database strings and other situations like that. 
+      catch (SqlException sqlException)
+      {
+        Log.SingleError("SitecoreOwin: sqlException doing garbage collect for EFAuthStore, is the database for the auth tokens well configured?. Exception: " + sqlException.Message, this);
+      }
     }
 
-    public class SqlAuthSessionStoreInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SQLAuthSessionStoreContext>
+    public Task<string> StoreAsync(AuthenticationTicket ticket)
     {
+      string key = Guid.NewGuid().ToString();
+      using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
+      {
+        _store.Entries.Add(new AuthSessionEntry { Key = key, TicketString = _formatter.Protect(ticket), ValidUntil = ticket.Properties.ExpiresUtc });
+        _store.SaveChanges();
+      }
+
+      return Task.FromResult(key);
     }
 
-    /// <summary>
-    /// Session store to be used with the OWIN cookie authentication middleware
-    /// </summary>
-    public class SqlAuthSessionStore : IAuthenticationSessionStore
+    public Task RenewAsync(string key, AuthenticationTicket ticket)
     {
-        private string _connectionString;
-        private TicketDataFormat _formatter;
-        private Timer _gcTimer;
-        public SqlAuthSessionStore(TicketDataFormat tdf, string cns = "AuthSessionStoreContext")
+      using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
+      {
+        AuthSessionEntry myEntry = _store.Entries.FirstOrDefault(a => a.Key == key);
+        if (myEntry != null)
         {
-            _connectionString = cns;
-            _formatter = tdf;
-            _gcTimer = new Timer(new TimerCallback(GarbageCollect), null, TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(15));
+          myEntry.TicketString = _formatter.Protect(ticket);
         }
-
-        private void GarbageCollect(object state)
+        else
         {
-            DateTimeOffset now = DateTimeOffset.Now.ToUniversalTime();
-            using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
-            {
-                foreach (var entry in _store.Entries)
-                {
-                    var expiresAt = _formatter.Unprotect(entry.TicketString).Properties.ExpiresUtc;
-                    if (expiresAt < now)
-                    {
-                        _store.Entries.Remove(entry);
-                    }
-                }
-                _store.SaveChanges();
-            }
+          _store.Entries.Add(new AuthSessionEntry { Key = key, TicketString = _formatter.Protect(ticket) });
         }
+        _store.SaveChanges();
 
-        public Task<string> StoreAsync(AuthenticationTicket ticket)
-        {
-            string key = Guid.NewGuid().ToString();
-            using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
-            {                
-                _store.Entries.Add(new AuthSessionEntry { Key = key, TicketString = _formatter.Protect(ticket), ValidUntil = ticket.Properties.ExpiresUtc });
-                _store.SaveChanges();                
-            }
-            
-            return Task.FromResult(key);
-        }
-
-        public Task RenewAsync(string key, AuthenticationTicket ticket)
-        {
-            using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
-            {
-                AuthSessionEntry myEntry = _store.Entries.FirstOrDefault(a => a.Key == key);
-                if (myEntry != null)
-                {
-                    myEntry.TicketString = _formatter.Protect(ticket);
-                }
-                else
-                {
-                    _store.Entries.Add(new AuthSessionEntry { Key = key, TicketString = _formatter.Protect(ticket) });
-                }
-                _store.SaveChanges();
-                
-            }
-            return Task.FromResult(0);
-        }
-
-        public Task<AuthenticationTicket> RetrieveAsync(string key)
-        {
-            AuthenticationTicket ticket = null;
-            using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
-            {
-                AuthSessionEntry myEntry = _store.Entries.FirstOrDefault(a => a.Key == key);
-                if (myEntry != null)
-                    ticket = _formatter.Unprotect(myEntry.TicketString);
-                return Task.FromResult(ticket);
-            }
-        }
-
-        public Task RemoveAsync(string key)
-        {
-            using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
-            {
-                AuthSessionEntry myEntry = _store.Entries.FirstOrDefault(a => a.Key == key);
-                if (myEntry != null)
-                {
-                    _store.Entries.Remove(myEntry);
-                    _store.SaveChanges();
-                }                
-            }
-
-            return Task.FromResult(0);
-        }
+      }
+      return Task.FromResult(0);
     }
+
+    public Task<AuthenticationTicket> RetrieveAsync(string key)
+    {
+      AuthenticationTicket ticket = null;
+      using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
+      {
+        AuthSessionEntry myEntry = _store.Entries.FirstOrDefault(a => a.Key == key);
+        if (myEntry != null)
+          ticket = _formatter.Unprotect(myEntry.TicketString);
+        return Task.FromResult(ticket);
+      }
+    }
+
+    public Task RemoveAsync(string key)
+    {
+      using (SQLAuthSessionStoreContext _store = new SQLAuthSessionStoreContext(_connectionString))
+      {
+        AuthSessionEntry myEntry = _store.Entries.FirstOrDefault(a => a.Key == key);
+        if (myEntry != null)
+        {
+          _store.Entries.Remove(myEntry);
+          _store.SaveChanges();
+        }
+      }
+
+      return Task.FromResult(0);
+    }
+  }
 
 
 }
