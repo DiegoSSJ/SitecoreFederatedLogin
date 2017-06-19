6bc8ae4f7f7d28b4441c4d8eed5c98591be2a511
Diego Saavedra San Juan <diego.saavedra.san.juan@liu.se>
2017-06-19 10:21:58 +0200

    Description

--- sitecoreOwinFederation/Properties/AssemblyInfo.cs
+++ sitecoreOwinFederation/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SitecoreOwinFederator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sitecore Owin module")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CA")]
 [assembly: AssemblyProduct("SitecoreOwinFederator")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
