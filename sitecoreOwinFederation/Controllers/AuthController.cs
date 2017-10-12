f9ec823af175bf2036047177ba0dfda52cfb166b
Diego Saavedra San Juan <diego.saavedra.san.juan@liu.se>
2017-10-12 10:47:59 +0200

    Support logout from Sitecore's logout facitlities, works partially, the page should be reloaded after logout, but the logout is effective even in Owin.

--- sitecoreOwinFederation/Controllers/AuthController.cs
+++ sitecoreOwinFederation/Controllers/AuthController.cs
@@ -7,12 +7,12 @@ using Sitecore;
 using Sitecore.Analytics;
 using Sitecore.Configuration;
 using Sitecore.Diagnostics;
-using Sitecore.Security.Authentication;
 using Sitecore.Sites;
 using Sitecore.StringExtensions;
 using Sitecore.Web;
-using Sitecore.Web.Authentication;
 using SitecoreOwinFederatorLiU.Pipelines.HttpRequest;
+using Sitecore.Pipelines;
+using Sitecore.Pipelines.Logout;
 
 namespace SitecoreOwinFederatorLiU.Controllers
 {
@@ -24,29 +24,17 @@ namespace SitecoreOwinFederatorLiU.Controllers
   {
     // GET: Auth
     [Authorize]
-    //public ActionResult Index(string sc_itemid, string sc_lang, string sc_db, string sc_device, string sc_mode, string sc_debug, string sc_trace, string sc_prof, string sc_ri, string sc_rb)
-    //public ActionResult Index(string[] parameters)
-    //{
-    //  Log.Info("SitecoreOwin Login with string[]",this);
-    //  //WebUtil.SetCookieValue(Constants.AdfsCurrentPathSaveCookieName, returnUrl);
-    //  return Index();
-    //}
-
-    //// GET: Auth
-    //[Authorize]
-    //public ActionResult Index(string returnUrl)
-    //{
-    //  Log.Debug("SitecoreOwin Login with returnUrl");
-    //  WebUtil.SetCookieValue(Constants.AdfsCurrentPathSaveCookieName, returnUrl);
-    //  return Index();
-    //}
-
-    // GET: Auth
-    [Authorize]
-    //public ActionResult Index()
     public ActionResult Index(string[] parameters)
     {
       Log.Debug("SitecoreOwin AuthController Index");
+
+        // Is Callback from Sitecore's logout, check user and do logout instead 
+      var fromSitecoreLogoutPipeline = HttpContext?.Request?.Params?.Get(Constants.LogoutFromSitecorePipelineParameterName);
+      if (!fromSitecoreLogoutPipeline.IsNullOrEmpty() && fromSitecoreLogoutPipeline.ToLower().Equals("true"))
+      {
+        Log.Debug("SitecoreOwing AuthController Index: Logging out user after callback from Sitecore's logout pipeline");
+        return Redirect(Constants.LogoutFromSitecoreAndSitecorePipelinePath);
+      }
 
       try
       {
@@ -81,13 +69,24 @@ namespace SitecoreOwinFederatorLiU.Controllers
           redirect = WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName);
         }
 
+        if (!Request.Params.Get("returnUrl").IsNullOrEmpty())
+        {
+          Log.Debug("SitecoreOwin: In AuthController login, found returnUrl request, using it instead of anything else");
+          redirect = Request.Params.Get("returnUrl");
+        }
+
         string rolesToRedirectToEdit = Settings.GetSetting(Constants.RolesToRedirectToEditSettingName, "");
         Log.Debug("SitecoreOwin: In AuthController login, rolesToRedirectToEdit is :" + rolesToRedirectToEdit);
         if (!rolesToRedirectToEdit.IsNullOrEmpty() &&
-            rolesToRedirectToEdit.Split('|').Any(role => Context.User.IsInRole(role)))
+            rolesToRedirectToEdit.Split('|').Any(role => Context.User.IsInRole(role)) &&
+            !redirect.Contains(Constants.SitecoreStartEditingParameter))
         {
-          Log.Debug("SitecoreOwin: In AuthController login, user matched roles to redirect to edit");
-          redirect += Constants.SitecoreStartEditingParameter;
+          string useParameterEditMode = Settings.GetSetting(Constants.UseParameterEditMode, "False");
+          Log.Debug("SitecoreOwin: In AuthController login, useParameterEditMode is " + useParameterEditMode);
+          if (useParameterEditMode.IsNullOrEmpty() || useParameterEditMode.ToLower().Equals("false"))
+            Context.Site.SetDisplayMode(DisplayMode.Edit, DisplayModeDuration.Remember);
+          else            
+            redirect += Constants.SitecoreStartEditingParameter;
         }
 
         Log.Debug("SitecoreOwin: In AuthController login, redirecting to " + redirect);
@@ -108,42 +107,103 @@ namespace SitecoreOwinFederatorLiU.Controllers
     {
       Log.Debug("SitecoreOwin AuthController Logout");
 
+
+      var redirect = GetRedirectTo();
+      var fromTransfer = HttpContext?.Request?.Params?.Get(Constants.LogoutTransferParameterName);
+      // Specifik till utloggning från Sitecore, fungerar inte ändå. Vi vill att sidan ska laddas om efter utloggningen, men det 
+      // fungerar inte. 
+      if ( !fromTransfer.IsNullOrEmpty() && fromTransfer.ToLower().Equals("true"))
+      {
+        Log.Debug("SitecoreOwin: Tranferring to " + redirect);
+        //Server.Transfer(redirect); // doesn't work
+        //Server.TransferRequest(redirect);
+        //Response.Redirect("/");
+        Response.Clear();
+        string responseText = Constants.DocumentLocationReloadHtml.Replace("\n", System.Environment.NewLine).Replace("#*#*", Request.Url.GetLeftPart(UriPartial.Authority) + redirect);        
+        Response.Output.Write(responseText);
+        //Response.Redirect("/", true);        
+        Response.End();
+        //Response.RedirectLocation =         
+        //Response.RedirectLocation = "/?hehe=true";
+        return null;
+
+      }
+
+      // Gör utloggningen via Sitecore's utloggningspipelines först, sedan i Owin
+      var fromSitecoreLogoutPipeline = HttpContext?.Request?.Params?.Get(Constants.LogoutFromSitecorePipelineParameterName);
+      if (fromSitecoreLogoutPipeline.IsNullOrEmpty() || !fromSitecoreLogoutPipeline.Equals("true"))
+      {
+        Log.Audit("SitecoreOwin: logging out user " + Context.User.Name + " from Sitecore", this);
+        var logoutArgs = new LogoutArgs();
+        logoutArgs.RedirectUrl = new Sitecore.Text.UrlString(Constants.LogoutFromSitecorePipelinePath);
+        try
+        {
+          Log.Debug("SitecoreOwin: trying to log out user from Sitecore via Content Editor logout pipeline");
+          CorePipeline.Run("logout", logoutArgs);
+        }
+        catch (InvalidOperationException ioe)
+        {
+          Log.Debug("SitecoreOwin: trying to log out user from Sitecore via EE logout pipeline");
+          CorePipeline.Run("speak.logout", logoutArgs);
+        }
+
+        Log.Debug("SitecoreOwin: logged out user from Sitecore, proceeding with Owin logout");
+        return Redirect(Constants.LogoutFromSitecorePipelinePath);
+      }
+
+
       // Change mode to normal to avoid being redirected to Sitecore loggin all the time. 
       Context.Site.SetDisplayMode(DisplayMode.Normal, DisplayModeDuration.Remember);
 
+
+
+
+      // Utloggad från Sitecore, men fortfarande inloggad via ADFS. Logga ut från ADFS via Owin. 
       if (Request.IsAuthenticated)
       {
-        Log.Audit("SitecoreOwin: Logging out user " + Context.User.Name, this);
 
-        string redirect = "/";
-        if (!WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName).IsNullOrEmpty() && Settings.GetBoolSetting(Constants.RedirectToCurrentLocationSettingName, false))
-          redirect = WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName);
+        Log.Audit("SitecoreOwin: Logging out user " + Context.User.Name + " from Owin", this);
+        if (redirect.Contains(Constants.SitecoreStartEditingParameter))
+          redirect = redirect.Replace(Constants.SitecoreStartEditingParameter, "");
 
-        AuthenticationProperties properties = new AuthenticationProperties();
+        
 
+        if (redirect.IndexOf(Constants.LogoutPath) == 0)
+          redirect = "/";
 
-        properties.RedirectUri = "https://" + Context.Site.TargetHostName + redirect;
-        properties.AllowRefresh = false;
-        AuthenticationManager.Logout();
-        foreach (AuthenticationProvider authenticationProvider in AuthenticationManager.Providers)
+        var fromSitecoreLogout = HttpContext?.Request?.Params?.Get(Constants.LogoutFromSitecoreParameterName);
+        if (!fromSitecoreLogout.IsNullOrEmpty() && fromSitecoreLogout.ToLower().Equals("true"))
         {
-          Log.Debug("SitecoreOwin: Logging out user " + Context.User.Name + " in provider: " + authenticationProvider.Name);
-          authenticationProvider.Logout();
+          // Vi loggade ut från Sitecore istället för vår utloggning, försök ladda om sidan i nästa steg (fungerar tyvärr inte än)
+          redirect = Constants.LogoutTransferPath + Uri.EscapeDataString(redirect);
         }
 
-        // Log out from Sitecore for real as we are using real users
-        Session.Abandon();
-        if (!TicketManager.GetCurrentTicketId().IsNullOrEmpty())
-          TicketManager.RemoveTicket(TicketManager.GetCurrentTicketId());
-        WebUtil.SetCookieValue(Constants.SitecoreUserTicketCookieName, "", DateTime.Now.AddDays(-1));
-
-        Log.Debug("SitecoreOwin: In AuthController logout, redirecting to " + redirect);
-        return Redirect(redirect);
-
+        Log.Debug("SitecoreOwin: In AuthController logout, redirecting to from Owin Logout " + redirect);
+        // Sign out from OWIN - redirect to current page
+        var owinContext = Request.GetOwinContext();
+        var authProperties = new AuthenticationProperties();
+        authProperties.RedirectUri = HttpContext.Request.Url.GetLeftPart(UriPartial.Authority) + redirect;
+        owinContext.Authentication.SignOut(authProperties);
       }
-      return Redirect("/");
+
+      return Redirect(redirect);
+    }
+
+
+    private string GetRedirectTo ()
+    {
+      string redirect = "/";
+      if (!WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName).IsNullOrEmpty() && Settings.GetBoolSetting(Constants.RedirectToCurrentLocationSettingName, false))
+        redirect = WebUtil.GetCookieValue(Constants.AdfsCurrentPathSaveCookieName);
+
+      if (!Request.Params.Get(Constants.SitecoreReturnUrlParameterName).IsNullOrEmpty())
+      {
+        Log.Debug("SitecoreOwin: In AuthController logout, found returnUrl request, using it instead of anything else");
+        redirect = Request.Params.Get("returnUrl");
+      }
+      return redirect;
     }
   }
-
-
 }
+
+
---
+++ sitecoreOwinFederation/pipelines/Logout/HandleSitecoreLogout.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Sitecore.Pipelines.Logout;
+using Sitecore.Web.Authentication;
+using System.Security.Claims;
+using System.Web;
+
+namespace SitecoreOwinFederatorLiU.pipelines.Logout
+{
+  public class HandleSitecoreLogout : PipelineArgs
+  {
+    /// <summary>Runs the processor to log out from Sitecore Owin when running sitecores logout pipeline</summary>
+    public void Process(LogoutArgs args)
+    {
+      Log.Debug("SitecoreOwin: in HandleSitecoreLogout");
+      args.RedirectUrl.Parameters.Add(Constants.LogoutFromSitecorePipelineParameterName, "true");
+      //Log.Debug("SitecoreOwin: in HandleSitecoreLogout");
+      //var thiss = this;
+      //var user = Sitecore.Context.User;
+      //var owinContext = HttpContext.Current.GetOwinContext();
+      //var owinContextRequest = HttpContext.Current.Request.GetOwinContext();
+      //var logonUserIdentity = HttpContext.Current.Request.LogonUserIdentity;
+      //var requestContextUser = HttpContext.Current.Request.RequestContext.HttpContext.User;
+      //string currentTicketId = TicketManager.GetCurrentTicketId();
+      //var ticketIds = TicketManager.GetTicketIDs();
+      //foreach (var ticketId in ticketIds)
+      //{
+      //  var thisTicket = TicketManager.GetTicket(ticketId);
+      //  Log.Debug("SitecoreOwin: ind HandleSitecoreLogout this ticket has username: " + thisTicket?.UserName);
+      //}
+      //var ticket = TicketManager.GetTicket(currentTicketId, true);
+      //var ticketUser = ticket?.UserName;
+      //var activeUser = Sitecore.Security.Authentication.AuthenticationManager.GetActiveUser();
+      //var accountUser = Sitecore.Security.Accounts.User.Current;
+      //var userFromClaims = ClaimsPrincipal.Current;
+      //var userFromMembership = System.Web.Security.Membership.GetUser();
+      //Log.Debug("SitecoreOwin: in HandleSitecoreLogout Request user: " + requestContextUser.Identity.Name);
+      //Log.Debug("SitecoreOwin: in HandleSitecoreLogout Claims user: " + userFromClaims?.Identity.Name);
+      //Log.Debug("SitecoreOwin: in HandleSitecoreLogout Owin Context user: " + HttpContext.Current.GetOwinContext().Authentication.User.Identity.Name);
+      //Log.Debug("SitecoreOwin: in HandleSitecoreLogout user from ticket: " + ticketUser);
+      //if (HttpContext.Current.GetOwinContext() != null && HttpContext.Current.GetOwinContext().Authentication != null &&
+      //  HttpContext.Current.GetOwinContext().Authentication.User != null && !Sitecore.Context.User.Domain.Equals("sitecore"))
+      //{
+      //  Log.Debug("SitecoreOwin: in HandleSitecoreLogout, user is logged in via Owin, logging out user");
+      //  //HttpContext.Current.GetOwinContext().Authentication.SignOut();
+      //}
+      return;
+    }
+
+  }
+}
